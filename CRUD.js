9443df7fd0aaf84c9763729f99ff0acc01ae12c2
paragSharma <parag0992.be21@chitkara.edu.in>
2023-11-27 18:09:37 +0530

    project completed

---
+++ CRUD.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const path = require("path");
+
+const filePath = path.join(__dirname, "./", "data.json");
+
+function getTodo() {
+    try {
+        const data = fs.readFileSync(filePath, { encoding: "utf-8" });
+        return data.length === 0 ? [] : JSON.parse(data);
+    } catch (err) {
+        console.error(err.message);
+        return null;
+    }
+}
+
+function addTodo(value) {
+    try {
+        const data = fs.readFileSync(filePath, { encoding: "utf-8" });
+        const todoData = data.length === 0 ? [] : JSON.parse(data);
+        todoData.push(value);
+        
+        fs.writeFileSync(filePath, JSON.stringify(todoData));
+        return  todoData;
+    } catch (err) {
+        console.error(err.message);
+        return null;
+    }
+}
+
+function newTodo(data){
+    
+    try{
+      fs.writeFileSync(filePath,JSON.stringify(data));
+    }catch(err){
+        console.error(err.message);
+        return null;
+    }
+}
+
+module.exports = { getTodo, addTodo , newTodo };
---
+++ index.js
@@ -0,0 +1,66 @@
+const  { getTodo, addTodo , newTodo } = require("./CRUD.js");
+
+const express = require("express");
+const app = express();
+app.use(express.urlencoded({extended:true}));
+app.use(express.json())
+
+
+
+app.get("/gettodo", (req,res)=>{
+  let data  = getTodo();
+  res.send(data);
+});
+
+
+app.post("/addtodo",(req,res)=>{
+  let taskitem = req.body;
+  console.log(taskitem);
+  let mssg= addTodo(taskitem);
+  res.send(mssg);
+})
+
+app.delete("/delete/:id" , (req , res)=>{
+    let data = getTodo();
+    const found = data.some(element=>element.id===parseInt(req.params.id))
+    
+    if(found){
+       for(let i=0; i<data.length; i++){
+           let element = data[i];
+           if(element.id==req.params.id){
+               data.splice(i,1); 
+               res.send(data);
+               newTodo(data);
+               return;
+            }
+       }
+       res.send(data);
+   }
+   else{
+     res.send({msg: `No member with the id ${req.params.id}`});
+   }
+
+});
+
+app.patch("/edit/:id" , (req , res)=>{
+    let data = getTodo();
+    let newData = req.body;
+    const found = data.some(element=>element.id===parseInt(req.params.id))
+
+   if(found){
+      data.forEach(element => {
+          if(element.id==req.params.id){
+          element.todo = newData.todo;
+          newTodo(data); 
+          }
+      });
+      res.send(data);
+   }
+   else{
+    res.send({msg: `No member with the id ${req.params.id}`});
+   }
+})
+
+app.listen(3000,()=>{
+    console.log("server started");
+})
